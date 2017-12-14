c381ed1b1fe93a3359e262677cfd403ae0ce5b90
dcarraro <dcarraro@ciandt.com>
2017-12-14 10:02:56 -0200

    removed solution

--- src/main/java/com/tdd/user/gateways/database/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tdd.user.gateways.database;
-
-import com.tdd.user.domains.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByDocument(String document);
-}
